da23b0eb7d53db5e842740479c442b1c396da1ef
Akelesis <marcus.tavares@outlook.com>
2020-03-02 15:21:47 -0300

    correcting json

--- src/components/restaurant/Table.vue
+++ src/components/restaurant/Table.vue
@@ -79,8 +79,16 @@
         <p>Dez porcento: R${{tip}}</p>
         <p>Total: R${{total}}</p>
       </div>
-      <b-button class="mt-3" variant="danger" block @click="closeTable">Fechar Mesa</b-button>
+      <b-button class="mt-3" variant="danger" block @click="$bvModal.show('close-table')">Fechar Mesa</b-button>
     </div>
+    <b-modal id="close-table" hide-footer>
+      <template slot="modal-title">Fechar Mesa - {{table.name}}</template>
+      <div class="d-block text-center">
+        <b-form-select v-model="paymentMethod" :options="paymentOpt" class="mb-3"></b-form-select>
+        <b-form-select v-model="creditMethod" :options="creditOpt" class="mt-3 mb-4"></b-form-select>
+      </div>
+      <b-button class="mt-3" variant="primary" block @click="closeTable()">Fechar Mesa</b-button>
+    </b-modal>
     
   </div>
 </template>
@@ -94,6 +102,22 @@ export default {
       stripped: ["light", "dark"],
       product: {},
       quantity: "",
+      paymentMethod: null,
+      creditMethod: 0,
+      paymentOpt: [
+        {value: null, text: "Escolha uma forma de pagamento"},
+        {value: "à vista", text: "À vista ou no débito"},
+        {value: "crédito", text: "Cartão de crédito"}
+      ],
+      creditOpt: [
+        {value: 0, text: "Escolha o numero de parcelas em caso de crédito"},
+        {value: 1, text: "1x"},
+        {value: 2, text: "2x"},
+        {value: 3, text: "3x"},
+        {value: 4, text: "4x"},
+        {value: 5, text: "5x"},
+        {value: 6, text: "6x"}
+      ],
       bill: 0.00,
       couvert: 0.00,
       total: 0.00,
@@ -130,7 +154,6 @@ export default {
         if(this.table.products[i].id == prod.id){
           this.table.products.splice(i,1)
         }
-        console.log(this.$store.state.tables)
       }
       this.$store.commit("removeProd", this.table)
       localStorage.setItem("_tables", JSON.stringify(this.$store.state.tables))
@@ -156,9 +179,95 @@ export default {
         const closedTable = {}
         const today = new Date()
         closedTable.bill = this.bill
-        closedTable.date = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDay()
-        closedTable.id = this.table.id
-        this.$store.commit('addClosedTable', closedTable)
+        closedTable.date = today.getUTCFullYear() + '-' + today.getUTCMonth() + '-' + today.getUTCDay() + 'T' + today.getUTCHours() + ":" + today.getUTCMinutes() + ":" + today.getUTCSeconds()
+        closedTable.date = closedTable.date + "-02:00"
+        let tableProds = []
+
+        for(let i = 0; i < this.$store.state.table.products.length; i++){
+          let aux = {}
+          aux.Codigo = this.$store.state.table.products[i].codeFromAPI
+          aux.Unidade = ""
+          aux.Descricao = this.$store.state.table.products[i].name
+          aux.Quantidade = this.$store.state.table.products[i].quantity
+          aux.ValorUnitario = this.$store.state.table.products[i].price
+          aux.DescontoUnitario = ""
+          aux.ValorTotal = this.$store.state.table.products[i].price * this.$store.state.table.products[i].quantity
+          tableProds.push(aux)
+          console.log(aux)
+        }
+
+        
+        
+        const headers = {
+          AuthorizationToken: "840feb502f48dc43eee47369ed251508960b49a3a3447222e711a103b08518b0a9a498e7069fc4f3763b990fa6d1480fdcf29f47ead5a9e67ad08327007eac3f72baabccc1b7d0ca086cc222a544c838195b400539aa2adf87c4a4557e434cb909b467ab0424a9f0b1dc9059cf774af3f168e9128b0427183271eb1cd8d40ca7",
+          User: "robson@milleniumcontabil.com",
+          App: "millenium"
+        }
+        const emission = {
+          "OrigemVenda":"Venda Direta",
+          "Deposito":"DEMONSTRAÇÃO TESTE",
+          "StatusSistema":"Orçamento",
+          "Status":"",
+          "Categoria":"",
+          "Validade":"",
+          "Empresa":"MILLENIUM CONTABIL",
+          "Cliente":"EMPORIO DA CARNE COMERCIO VAREJISTA EIRELI",
+          "Vendedor":"login@vendedor.com",
+          "LancarComissaoVendedor":true,
+          "PlanoDeConta":"ALIMENTACAO",
+          "FormaPagamento":"",
+          "NumeroParcelas":0,
+          "Transportadora":"",
+          "DataEnvio":"0001-01-01T00:00:00-02:00",
+          "Enviado":false,
+          "ValorFrete":0.0,
+          "FreteContaEmitente":false,
+          "ValorSeguro":0.0,
+          "Descricao":"",
+          "OutrasDespesas":0.0,
+          "ValorFinal": 0,
+          "Finalizado":false,
+          "Lancado":false,
+          "NumeroNFe":"1234",
+          "Municipio":"Itabuna",
+          "CodigoMunicipio":"4302352",
+          "Pais":"Brasil",
+          "CEP":"95765000",
+          "UF":"RS",
+          "UFCodigo":"43",
+          "Bairro":"",
+          "Logradouro":"Rua São José",
+          "LogradouroNumero":"204",
+          "LogradouroComplemento":"Casa",
+          "Items": tableProds,
+          "Pagamentos": [
+            {
+            "DescricaoPagamento":"Pagamento",
+            "ValorPagamento":10.0,
+            "BandeiraCartao":"99",
+            "NumeroTerminal":"1",
+            "DataTransacao":"2019-08-12T19:14:38-02:00",
+            "CredenciadoraCartao":"",
+            "CredenciadoraCNPJ":"",
+            "CV_NSU":"",
+            "CondicaoPagamento":1,
+            "Parcelas":0,
+            "PeriodoParcelas":0,
+            "Adiantamento":0.0,
+            "Quitar":"true"
+            }]
+          }
+          console.log({...emission})
+
+          const pack = {headers, emission}
+
+        axios.post('http://localhost:8080/api/bills', pack)
+        .then(res => {
+          console.log(res.data)
+        })
+        
+        /*closedTable.id = this.table.id
+        this.$store.commit('addClosedTable', closedTable)*/
 
     }
   },
